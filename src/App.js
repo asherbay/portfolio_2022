ad805d13d998e67bbb3ed9b6be8271014b1988be
Asher Bay <asherbay@gmail.com>
2022-02-22 12:10:44 -0700

    Revert "flex layout"
    This reverts commit e6c7c3eaa6558592b6302ccded1548f4232eb6a5.

--- src/App.js
+++ src/App.js
@@ -6,20 +6,13 @@ import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Resume from './pages/Resume'
 import Contact from './pages/Contact'
-import React, {useState, useEffect} from 'react'
-import p5 from 'p5'
+import React from 'react'
 
 function App() {
-  const [zoom, setZoom] = useState(1)
-  p5.windowResized = () => {
-    console.log('zoom set')
-    setZoom(p5.map(p5.windowWidth, 600, 1600, 0.3, 0.7))
-  }
-
   return (
-    <div style={{background: "#656565",}}>
-      
-      <Routes  >
+    <div style={{backgroundColor: "gray", display: 'flex', justifyContent: 'center', alignItems: 'flex-start'}}>
+      <Nav />
+      <Routes >
         <Route path='/' element={<Home/>}/>
         <Route path='/projects' element={<Projects/>}/>
         <Route path='/resume' element={<Resume/>}/>
